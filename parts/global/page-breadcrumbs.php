2c039348cb98bc324e89cb380239531aeb93308b
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-07-10 15:04:39 +0100

    Fix breadcrumbs to have ellipses for overflow.

--- parts/global/page-breadcrumbs.php
+++ parts/global/page-breadcrumbs.php
@@ -2,7 +2,7 @@
 	<div class="container-fluid">	
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">	
-				<div id="breadcrumbs-inner">	
+				<div class="breadcrumbs-inner">	
 					<?php if(function_exists('bcn_display')) { bcn_display(); }?>
 				</div>
 			</div>
