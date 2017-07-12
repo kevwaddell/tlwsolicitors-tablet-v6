f5e0f9dd809e99363b272041cdda8413352b56b4
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-07-12 09:38:30 +0100

    Update media section on sitemap

--- parts/sitemap/column-left.php
+++ parts/sitemap/column-left.php
@@ -17,21 +17,18 @@ $downloads_pg_id = get_page_id('downloads');
 
 <div class="col-xs-6">
 	
-	<?php if ($media_pages) { ?>
+	<h3>TLW Media</h3>
+
+	<div class="list-block">
+
+		<ul class="list-unstyled">
 		
-		<h3>TLW Media</h3>
-	
-		<div class="list-block">
-	
-			<ul class="list-unstyled">
+			<li><a href="<?php echo get_permalink($videos_pg_id); ?>"><?php echo get_the_title($videos_pg_id); ?></a></li>
+			<li><a href="<?php echo get_permalink($downloads_pg_id); ?>"><?php echo get_the_title($downloads_pg_id); ?></a></li>
 			
-				<li><a href="<?php echo get_permalink($videos_pg_id); ?>"><?php echo get_the_title($videos_pg_id); ?></a></li>
-				<li><a href="<?php echo get_permalink($downloads_pg_id); ?>"><?php echo get_the_title($downloads_pg_id); ?></a></li>
-				
-			</ul>
-			
-		</div>
-	<?php } ?>
+		</ul>
+		
+	</div>
 	
 	<?php if ($services) { ?>
 	
