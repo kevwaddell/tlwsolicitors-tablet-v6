ae75b026b60d4f5ec9f09953b5e63d939a447729
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-07-21 11:07:33 +0100

    Remove video viewer files and styles.

--- parts/global/popups/all-popups.php
+++ parts/global/popups/all-popups.php
@@ -3,7 +3,5 @@
 <?php get_template_part( 'parts/global/popups/search', 'form' ); ?>
 		
 <?php get_template_part( 'parts/global/popups/route', 'finder' ); ?>
-
-<?php get_template_part( 'parts/global/popups/video', 'viewer' ); ?>
 		
 <?php get_template_part( 'parts/global/popups/xmas', 'popup' ); ?>
