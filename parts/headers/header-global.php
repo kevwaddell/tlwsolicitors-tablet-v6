50d4c04889cde87b55b8b45709f6f196bf6cced9
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-01-18 13:44:55 +0000

    Add Trinity Mirror google scripts

--- parts/headers/header-global.php
+++ parts/headers/header-global.php
@@ -3,9 +3,20 @@
 <head id="www-tlwsolicitors-co-uk" data-template-set="tlw-solicitors-theme">
 	
 	<?php if ($_SERVER['SERVER_NAME']=='www.tlwsolicitors.co.uk') { ?>
+	<!-- Trinity Mirror script -->
+	<script>
+!function(t,e){e||(e=e||{},window.permutive=e,e.q=[],_pv="1.0",e.init=function(n,r,o){e.config={projectId:n,apiKey:r,environment:o||"production"};for(var i=["track","identify","events","query","addon","trigger","ready","defineEntities"],a=0;a<i.length;a++){var c=i[a];e[c]=function(t){return function(){var n=Array.prototype.slice.call(arguments,0);e.q.push({functionName:t,arguments:n})}}(c)}var p=t.createElement("script");p.type="text/javascript",p.async=!0;var s="https:"==t.location.protocol?"https://d3alqb8vzo7fun.cloudfront.net":"http://cdn.permutive.com";p.src=s+"/"+n+"-"+_pv+".js";var u=t.getElementsByTagName("script")[0];u.parentNode.insertBefore(p,u)})}(document,window.permutive),permutive.init("8a2c2f13-f564-4fff-8d22-177534028b95","68303c0c-2e2e-4b14-8d14-245e59740f1a");
+	</script>
 	<!-- Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PLBR4F');</script>
 	<!-- End Google Tag Manager -->
+	<!-- Trinity Mirror Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KHL78WL');</script>
+	<!-- End Trinity Mirror Google Tag Manager -->
 		<!-- Adwords Call tracking -->
 	<script type="text/javascript">
 	(function(a,e,c,f,g,h,b,d){var k={ak:"966769174",cl:"lASLCOjY32sQlvTzAM",autoreplace:"08001695925"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[g]|| (a[g]=k.ak);b=e.createElement(h);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(h)[0];d.parentNode.insertBefore(b,d);a[f]=function(b,d,e){a[c](2,b,k,d,null,new Date,e)};a[f]()})(window,document,"_googWcmImpl","_googWcmGet","_googWcmAk","script");
